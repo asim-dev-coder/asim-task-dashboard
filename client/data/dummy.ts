c0921b95a58b16dd69d468b6b928de408c75a931
Assim <asimankon@gmail.com>
2025-08-08 20:35:46 +0600

    Edited and updated styIes

--- client/data/dummy.ts
+++ client/data/dummy.ts
@@ -46,7 +46,7 @@ export const dummyUsers: User[] = [
   {
     id: '1',
     name: 'Assim Ettisum',
-    email: 'asim@asimtask.com',
+    email: 'asim@outlook.com',
     avatar: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?w=40&h=40&fit=crop&crop=face',
     role: 'CEO'
   },
